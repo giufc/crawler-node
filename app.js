b9eadc5e5795fcd85ecdb8f0ec2143365ac24f97
Giuliano <giulianofc@P.local>
2017-05-23 16:09:40 -0300

    Teste

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var app = express();
-var http = require('http')
+var http = require('http');
+
+function startServer () {
+    var serverPort = process.env.PORT || 8080;
+    http.createServer(app).listen(serverPort, null, null, function(){
+        console.log("Listening to http port " + serverPort);
+    });
+}
+
+startServer();
 
 // index page
 app.get('/health', function(req, res) {
-    res.sendStatus(200)
+    res.sendStatus(200);
+    console.log("Funcionando");
 });
-
-function startServer () {
-    var serverPort = process.env.PORT || 8080
-    http.createServer(app).listen(serverPort, null, null, function(){
-        console.log("Listening to http port " + serverPort)
-    })
-}
-
-startServer()
